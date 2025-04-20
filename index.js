f0fcb8de5ceb6708a21a8d329d443d0cab83a398
Kaveesha <kalshandhanasekara@gmail.com>
2025-04-20 15:40:31 +0530

    some modification

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 import express from "express";
 import jsonServer from "json-server";
 import auth from "json-server-auth";
+import { createServer } from "vercel-serverless-express";
 
 const server = express();
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+});
 
-// Add a route to show a message at the root URL
+// message at the root URL
 server.get('/', (req, res) => {
     res.send('Welcome to the eBookNest API!');
 });
 
 const router = jsonServer.router('./data/db.json');
 server.use('/api', router);
-server.db = router.db
+server.db = router.db;
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 const rules = auth.rewriter({
     products: 444,
     featured_products: 444,
@@ -26,11 +27,10 @@ const rules = auth.rewriter({
     users: 600
 });
 
-server.use(rules)
-server.use(auth)
-server.use(middlewares)
-server.use(router)
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
+server.use(router);
 
-server.listen(8000, () => {
-    console.log('JSON Server is running on port 8000');
-});
+// Export the server as a Vercel serverless function
+export default createServer(server);
